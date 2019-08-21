8d3db50765009e532b8381bb8b2220538dae3cd1
dev4us <bronxofsoul@gmail.com>
2019-08-21 23:04:38 +0900

    [BE] Connect Prisma & merge type and resolvers for GraphQL

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,26 +1,16 @@
 require("dotenv").config();
 
 import { GraphQLServer } from "graphql-yoga";
+import logger from "morgan";
+import schema from "./schema";
 
 const PORT = process.env.PORT || 4000;
 
-const typeDefs = `
-  type Query{
-    hello: String!
-  }
-`;
-
-const resolvers = {
-  Query: {
-    hello: () => "Hi, there!"
-  }
-};
-
 const server = new GraphQLServer({
-  typeDefs,
-  resolvers
+  schema
 });
 
+server.express.use(logger("dev"));
 server.start({ port: PORT }, () =>
-  console.log(`Server running on http://localhost:${PORT}`)
+  console.log(`😝 Server running on http://localhost:${PORT}`)
 );
