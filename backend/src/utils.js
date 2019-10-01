6880059d073eb59aa0f4d411ce545a7d657cfe5c
dev4us <bronxofsoul@gmail.com>
2019-10-01 23:29:10 +0900

    # 3.3 sendMail function with Nodemailer

--- backend/src/utils.js
+++ backend/src/utils.js
@@ -1,6 +1,36 @@
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.resolve(__dirname, ".env") });
+
 import { adjectives, nouns } from "./words";
+import nodemailer from "nodemailer";
+import sgTransport from "nodemailer-sendgrid-transport";
 
 export const generateScrect = () => {
   const randomNumber = Math.floor(Math.random() * adjectives.length);
   return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
+};
+
+export const sendMail = email => {
+  const options = {
+    auth: {
+      api_user: process.env.SENDGRID_USERNAME,
+      api_key: process.env.SENDGRID_PASSWORD
+    }
+  };
+
+  const client = nodemailer.createTransport(sgTransport(options));
+  return client.sendMail(email);
+};
+
+export const sendSecretMail = (address, secret) => {
+  const email = {
+    from: "admin@prismagram.com",
+    to: address,
+    subject: "Login Secret for instagram_graphql",
+    html: `Hello! Your Login Secret it ${secret}.<br/> Copy paste on the app/website to log in`
+  };
+
+  return sendMail(email);
 };
