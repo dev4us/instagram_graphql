baf8982d5b6250e46b1426c26a28447aef126dad
dev4us <bronxofsoul@gmail.com>
2019-12-03 23:51:04 +0900

    # 3.17 isLiked Computed File

--- backend/src/api/User/computeds.js
+++ backend/src/api/User/computeds.js
@@ -19,5 +19,26 @@ export default {
 
       return user.id === parentId;
     }
+  },
+  Post: {
+    isLiked: async (parent, _, { request }) => {
+      const { user } = request;
+      const { id } = parent;
+
+      return prisma.$exists.like({
+        AND: [
+          {
+            user: {
+              id: user.id
+            }
+          },
+          {
+            post: {
+              id
+            }
+          }
+        ]
+      });
+    }
   }
 };
